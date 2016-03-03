2a6a5d8972834d7221ad0011d6cbf2f1ec5c9479
Julien Schneider <julien.schneider@he-arc.ch>
2016-03-03 20:52:17 +0100

    Premier commit

---
+++ src/ch/hearc/ig/odi/customeraccount/App/Main.java
@@ -0,0 +1,46 @@
+package ch.hearc.ig.odi.customeraccount.App;
+
+import ch.hearc.ig.odi.customeraccount.business.Account;
+import ch.hearc.ig.odi.customeraccount.business.Customer;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author silvio.gutierre
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Customer c1 = new Customer(0,"Silvio","Gutierrez");
+        Customer c2 = new Customer(1,"Ajtene","Kurtaliqi");
+        
+        Account a1 = new Account("0","Compte Epargne",0.21,c1);
+        Account a2 = new Account("1","Compte Courant",0.4,c1);
+        Account a3 = new Account("2","Compte Epargne",0.05,c2);
+
+        a1.debit(1000);
+        System.out.println("a1 solde:"+a1.getBalance());
+        a2.debit(1495);
+        System.out.println("a2 solde:"+a2.getBalance());
+        a3.debit(3400);
+        System.out.println("a3 solde:"+a3.getBalance());
+        a3.credit(3600);
+        System.out.println("a3 solde:"+a3.getBalance());
+        
+        Account.transfer(300, a2, a1);
+        
+        System.out.println("a1 solde:"+a1.getBalance());
+        System.out.println("a2 solde:"+a2.getBalance());
+        
+    }
+    
+}
---
+++ src/ch/hearc/ig/odi/customeraccount/business/Customer.java
@@ -0,0 +1,92 @@
+package ch.hearc.ig.odi.customeraccount.business;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Customer {
+
+	private int number;
+	private String firstName;
+	private String lastName;
+        private List<Account> accounts;
+	/**
+	 * 
+	 * @param number
+	 * @param firstName
+	 * @param lastName
+	 */
+	public Customer(int number, String firstName, String lastName) {
+            this.number = number;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            accounts = new ArrayList();
+	}
+
+	public int getNumber() {
+		return this.number;
+	}
+
+	/**
+	 * 
+	 * @param number
+	 */
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	/**
+	 * 
+	 * @param firstName
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	/**
+	 * 
+	 * @param lastName
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * 
+	 * @param number
+	 */
+	public Account getAccountByNumber(String number) {
+            Account r_account = null;
+            boolean found = false;
+            int i = 0;
+            while(i<accounts.size() && !found){
+                if(accounts.get(i).getNumber().equals(number)){
+                    r_account = accounts.get(i);
+                    found = true;
+                }
+                i++;
+            }
+            
+            return r_account;
+            
+	}
+
+	/**
+	 * 
+	 * @param number
+	 * @param name
+	 * @param rate
+	 */
+	public void addAccount(String number, String name, double rate) {
+            accounts.add(new Account(number,name,rate,this));
+	}
+
+}
